5ccd74a25454b0e2a8193c73e7e355e8b7d34244
Vinícius Souza <viniciuspodi@gmail.com>
2018-03-13 23:08:21 -0400

    Desafio da Calculadora + Html e CSS do CRUD

---
+++ desafio2/js/script.js
@@ -0,0 +1,7 @@
+function abrirMenu(){
+    document.getElementById("menu").classList.add("menu-aberto");
+}
+
+function fecharMenu(){
+    document.getElementById("menu").classList.remove("menu-aberto");
+}
